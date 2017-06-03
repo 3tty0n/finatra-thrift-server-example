fea0e2e8675b39e3ed8403ebaddd9f599c11fa4f
Yusuke Izawa <yuizalp@gmail.com>
2017-06-04 00:20:44 +0900

    format

--- client/src/main/scala/com/example/ExampleClient.scala
+++ client/src/main/scala/com/example/ExampleClient.scala
@@ -9,19 +9,22 @@ object ExampleClient extends App {
   val host = "localhost:9911"
 
   val clientServiceIface: PingService.ServiceIface =
-    Thrift.client.newServiceIface[PingService.ServiceIface](host, "thrift_client")
+    Thrift.client
+      .newServiceIface[PingService.ServiceIface](host, "thrift_client")
 
   val client: PingService.FutureIface =
     Thrift.client.newIface[PingService.FutureIface](host)
 
-  client.echo("hoge") onSuccess { s => println(s"Reponse: " + s )}
-  client.ping() onSuccess { s => println(s"Response: " + s)}
+  client.echo("hoge") onSuccess { s =>
+    println(s"Reponse: " + s)
+  }
+  client.ping() onSuccess { s =>
+    println(s"Response: " + s)
+  }
 
-  Await.result(
-    for {
-      _ <- client.ping()
-      _ <- client.echo("hgoe")
-    } yield ()
-  )
+  Await.result(for {
+    _ <- client.ping()
+    _ <- client.echo("hgoe")
+  } yield ())
 
 }
