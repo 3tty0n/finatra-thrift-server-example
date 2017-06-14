4ea7a79ace27829767bd33c39db1f879f50249bd
Yusuke Izawa <yuizalp@gmail.com>
2017-06-14 23:25:29 +0900

    format ping.thrift

--- idl/src/main/thrift/ping.thrift
+++ idl/src/main/thrift/ping.thrift
@@ -6,14 +6,17 @@ include "finatra-thrift/finatra_thrift_exceptions.thrift"
 
 service PingService {
 
-	/**
-	 * Respond with 'pong'
-	 */
+  /**
+   * Respond with 'pong'
+   */
   string ping() throws (
     1: finatra_thrift_exceptions.ClientError clientError,
     2: finatra_thrift_exceptions.ServerError serverError
   )
 
+  /**
+   * ehco service
+   */
   string echo(
     1: string a
   ) throws (
